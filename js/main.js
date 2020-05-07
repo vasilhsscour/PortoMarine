e54bddb18eb154765faf36483f29cd5745806e22
vasilhsscour <vasilhsscour@gmail.com>
2020-05-07 22:46:49 +0300

    Add Owl Carousel to the Dishes Section

--- js/main.js
+++ js/main.js
@@ -57,4 +57,25 @@ $(function () {
         }
     });
 
+    $("#dishes-wrap").ready(function () {
+        $(".owl-carousel").owlCarousel({
+            items: 3,
+            autoplay: true,
+            smartSpeed: 700,
+            loop: true,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                }
+            }
+        });
+    });
+
 });
